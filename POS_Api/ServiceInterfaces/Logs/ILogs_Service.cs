98dc6e93ff7650d2d2519527ea63e3715f725774
SomethingFischer <63812807+SomethingFischer@users.noreply.github.com>
2026-04-30 09:59:12 +0200

    Commit

---
+++ POS_Api/ServiceInterfaces/Logs/ILogs_Service.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_Api.ServiceInterfaces.Logs
+{
+    public interface ILogs_Service
+    {
+    }
+}
---
+++ POS_Api/Services/Logs/Logs_Custom_Service.cs
@@ -0,0 +1,21 @@
+using Microsoft.Data.SqlClient;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMIS_Common.Sql;
+
+using POS_Common.Models.Logs.POS_Logs;
+using POS_Api.Services.Logs;
+
+namespace POS_Api.Services
+{
+    public class Logs_Custom_Service : Logs_Custom_SP_Service
+    {
+        #region Properties
+        #endregion
+    }
+}
